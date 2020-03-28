cbf4a01e33aae41346b13f48b56094ebfd6175d5
Taja House <ahlliahtaja@yahoo.com>
2020-03-27 21:59:30 -0700

    Setting up and learning class components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+constructor(){
+  super();
+  this.state = {
+    greet: "Hi Taja!"
+  }
+}
+
+  render() {
+    return(
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+              {this.state.greet}
+              </p>
+              <button 
+              onClick={() => this.setState(
+                { greet: 'Welcome back Taja!' })}
+                > 
+                Click Here!
+              </button>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+    )
+  }
 }
 
 export default App;
