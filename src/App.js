fe33b839c93424389317820560d25560e0b97c01
Taja House <ahlliahtaja@yahoo.com>
2020-03-30 15:15:06 -0700

    Adding files to github pages

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+
+import {CardList} from './components/card-list/component.card-list';
+
+import {SearchBox} from './components/search-box/search-box.component';
+
 import './App.css';
 
-class App extends Component{
-constructor(){
+
+class App extends Component {
+constructor() {
   super();
+
   this.state = {
-    greet: "Hi Taja!"
-  }
+    monsters: [],
+    searchField: ''
+};
+
+  // this.handleChange = this.handleChange.bind(this); <- So instead of creating handlelChange(e){}, I made it into an arrow function which eliminated the need to write this.handleChange method. This way, we don't have to bind every class method that is written.
+
 }
 
+componentDidMount(){
+  fetch('http://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({monsters : users})
+    );
+}
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value })
+  }
+
+
   render() {
-    return(
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-              {this.state.greet}
-              </p>
-              <button 
-              onClick={() => this.setState(
-                { greet: 'Welcome back Taja!' })}
-                > 
-                Click Here!
-              </button>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    return (
+          <div className='App'>
+            
+          <h1>Monsters Rolodex </h1>
+          <SearchBox 
+            placeholder= 'search monsters'
+            handleChange= {this.handleChange}
+            />
+
+          <CardList monsters = {filteredMonsters}/ >
           </div>
-    )
+    );
   }
 }
 
