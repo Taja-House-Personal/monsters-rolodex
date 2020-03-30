cf4bc872f56bb9ccbdbcaac35d27babfb0e895a1
Taja House <ahlliahtaja@yahoo.com>
2020-03-30 15:14:36 -0700

    Updates

---
+++ precache-manifest.6028f6149fe638746b87307bd730ba78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454263e3e63161fec055b158fd345bb9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "94866d6ba5beba619060",
+    "url": "/monsters-rolodex/static/css/main.d5a7613f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94866d6ba5beba619060",
+    "url": "/monsters-rolodex/static/js/main.1417b121.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
